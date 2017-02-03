42effb252620aec7cfcf6a7ac9d0b23654cdbdd9
Thuan Nguyen <thuannguyen@Thuans-MacBook-Pro.local>
2017-02-02 18:11:40 -0800

    first commit

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Thuan Nguyen on 2/2/17.
+//  Copyright © 2017 Thuan Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var rateLabel: UILabel!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- MovieViewer/ViewController.swift
+++ MovieViewer/ViewController.swift
@@ -7,18 +7,87 @@
 //
 
 import UIKit
+import AFNetworking
+import MBProgressHUD
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var movies: [NSDictionary]?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
+    
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+        let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+            MBProgressHUD.hide(for: self.view, animated: true)
+            if let data = data {
+                if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                    print(dataDictionary)
+                 self.movies = dataDictionary["results"] as! [NSDictionary]
+                self.tableView.reloadData()
+                    
+                    
+                }
+            }
+        }
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        if let movies = movies{
+            return movies.count
+        }
+        else{
+        return 0
+        }
+        
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        let movie = movies![indexPath.row]
+        let title = movie["title"] as! String
+        cell.titleLabel.text = title
+        
+        let overview = movie["overview"] as! String
+        cell.overviewLabel.text = overview
+        let rating = movie["vote_average"] as! Double
+        cell.rateLabel.text = (String)(rating)
+        let baseURL = "https://image.tmdb.org/t/p/w500/"
+        let posterPath = movie["poster_path"] as! String
+        let imageURL = NSURL(string: baseURL + posterPath)
+        cell.posterView.setImageWith(imageURL as! URL)
+        
+        
+        
+        return cell
+    }
+    func refreshControlAction(_ refreshControl: UIRefreshControl){
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+       // let task: URLSessionDataTask = session.dataTask(with: URLRequest) { (data: Data?, response: URLResponse?, error: Error?) in
+            self.tableView.reloadData()
+            refreshControl.endRefreshing()
+
+    }
+    
 
 
 }
